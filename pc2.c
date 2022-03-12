499f7c01aaf898dc2ed650ab651f82c72ca3877c
andres0599 <57111052+andres0599@users.noreply.github.com>
2022-03-11 22:22:10 -0600

    Add files via upload

---
+++ pc2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+  int i;
+  for (i = 1; i < argc; i++) {
+    unsigned char value = argv[i][0];
+  }
+
+  return 0;
+}
+
+//Estaba en clases y me dolia la cabeza. 
